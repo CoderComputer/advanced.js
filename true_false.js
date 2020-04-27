e01787b412b3619e63a302c19378c8fa2454bfdb
CoderComputer <codercomputer2021@gmail.com>
2020-04-27 20:01:37 +0600

    true vs false

--- true_false.js
+++ true_false.js
@@ -1,5 +1,14 @@
-const age = 0;
-const name = " "
+// false
+// const age = 0;
+// const name = ""
+// undefined
+// null
+// NaN
+
+// Truthy
+// "0" , " ",{},[]
+let name = true;
+console.log(name)
 if(name){
     console.log("Condition is true");
 }
