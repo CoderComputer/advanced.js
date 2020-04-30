49946ce696a33dd89bb0e88c58a98e2b0c87ebdb
CoderComputer <codercomputer2021@gmail.com>
2020-05-01 00:52:29 +0600

    scope problem done

---
+++ scope.js
@@ -0,0 +1,12 @@
+function myFun(num1, num2) {
+    const addValue = num1 + num2;
+    if (15>20) {
+        console.log("True")
+    } else {
+        var name = "Arko"
+        console.log(name)
+    }
+    console.log(name)
+    return addValue;
+}
+console.log(myFun(15,20))
